2ca6aed236b5aff7b87a286c41d7d573baf49679
zihongZHOU <41744038+zihongZHOU@users.noreply.github.com>
2022-07-08 15:17:26 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/_STM32F103C8T6_Read_AS5047/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F103C8T6_Read_AS5047' 
+ * Target:  'STM32F103C8T6_Read_AS5047' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/STM32F103C8T6_Read_AS5047/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+stm32f103c8t6_read_as5047\startup_stm32f103xb.o: startup_stm32f103xb.s
